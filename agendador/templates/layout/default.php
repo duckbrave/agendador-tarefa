facbd9a9923eda5ea5a0a62ff9f637e38f62d347
Thiago Emanuel <thiag0224@hotmail.com>
2025-06-17 21:11:28 -0300

    Mexendo na estilização de css para o projeto

--- agendador/templates/layout/default.php
+++ agendador/templates/layout/default.php
@@ -28,7 +28,8 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->Html->meta('icon') ?>
 
     <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
-
+    <?= $this->Html->css('custom') ?>
+    
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
@@ -36,11 +37,7 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 <body>
     <nav class="top-nav">
         <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+            <a href="<?= $this->Url->build('/') ?>"><span>Agendador de </span>Tarefas</a>
         </div>
     </nav>
     <main class="main">
